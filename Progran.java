1492b7b63df66f9046d0a4073c1eb15579c808c1
SantoshBiradar5676 <77573804+SantoshBiradar5676@users.noreply.github.com>
2021-11-25 14:31:51 +0530

    Add files via upload

---
+++ Progran.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class Main
+{
+    public static void main(String args[])
+    {
+        int n, size=100;
+        int[][]arr=new int[size][size];
+        Scanner in= new Scanner(System.in);
+        System.out.println("Matrix is");
+    
+        for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+		   	 arr[i][j]=in.nextInt();
+         }}
+		 		
+		System.out.println("Transpose of given matrix is:");
+        for(int i=0;i<3;i++)
+		{
+	   	  	for(int j=0;j<3;j++)
+			{
+ 				System.out.print(arr[j][i]+" ");
+			}
+			System.out.println();
+		}  	
+
+        
+    }
+}
